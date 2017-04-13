eebc4d950ff2ee28ad070a98defab98496f88b78
Bill Wu <bill.wu@autodesk.com>
2017-04-13 12:06:43 +0800

    Add a middleware for logger.

--- todos-react-js/src/index.js
+++ todos-react-js/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 
 import { Utils } from './components/Utils';
 import rootReducer from './reducers'
 import { SHOW_ALL } from "./constants/TodoFilters";
+import { logger, crashReporter } from "./middlewares/logger"
 
 import App from './containers/App'
 // import { style } from '../css/base.css';
@@ -15,10 +16,11 @@ let preloadedState = {
     todos: Utils.store('react-todos'),
     showFilter: SHOW_ALL
 };
-const store = createStore(rootReducer, preloadedState)
-
-// output the state log.
-applyMiddlewareByMonkeypatching(store, [ logger, crashReporter ]);
+const store = createStore(
+    rootReducer,
+    preloadedState,
+    applyMiddleware(logger, crashReporter)
+)
 
 //
 ReactDOM.render(
@@ -27,41 +29,3 @@ ReactDOM.render(
     </Provider>,
     document.getElementById('container')
 );
-
-
-//
-function logger(store) {
-    let next = store.dispatch
-    return function dispatchAndLog(action) {
-        console.log('dispatching', action)
-        let result = next(action)
-        console.log('next state', store.getState())
-        return result
-    }
-}
-
-function crashReporter(store) {
-    let next = store.dispatch
-    return function dispatchAndReportErrors(action) {
-        try {
-            return next(action)
-        } catch (err) {
-            console.error('ERROR:', err)
-            console.log({
-                action,
-                state: store.getState()
-            });
-
-            throw err
-        }
-    }
-}
-
-function applyMiddlewareByMonkeypatching(store, middlewares) {
-    middlewares = middlewares.slice()
-    middlewares.reverse()
-
-    middlewares.forEach(middleware =>
-        store.dispatch = middleware(store)
-    )
-}
