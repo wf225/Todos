86090d0510d1e0512f769842810f0114ff5cc8d5
Bill Wu <bill.wu@autodesk.com>
2017-04-17 19:29:48 +0800

    Add configureStore and sagaMonitor.

--- todos-react-js/src/index.js
+++ todos-react-js/src/index.js
@@ -4,38 +4,15 @@ import { Provider } from 'react-redux'
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import 'babel-polyfill'
 import createSagaMiddleware from 'redux-saga'
-
-import { Utils } from './components/Utils';
-import rootReducer from './reducers'
-import { SHOW_ALL } from "./constants/TodoFilters";
-import { logger, crashReporter } from "./middlewares/logger"
-
 // import { style } from '../css/base.css';
 
 import App from './containers/App'
+import configureStore from './store/configureStore'
 import { rootSaga } from './middlewares/sagas'
 
-//
-let preloadedState = {
-    todos: Utils.store('react-todos'),
-    showFilter: SHOW_ALL
-};
+const store = configureStore();
+store.runSaga(rootSaga);
 
-// create the saga middleware
-const sagaMiddleware = createSagaMiddleware();
-
-// mount it on the Store
-const store = createStore(
-    rootReducer,
-    preloadedState,
-    // applyMiddleware(logger, crashReporter, sagaMiddleware)
-    applyMiddleware(sagaMiddleware)
-);
-
-// then run the saga
-sagaMiddleware.run(rootSaga)
-
-//
 ReactDOM.render(
     <Provider store={store}>
         <App />
---
+++ todos-react-js/src/store/configureStore.js
@@ -0,0 +1,28 @@
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+
+import { Utils } from '../components/Utils';
+import { SHOW_ALL } from "../constants/TodoFilters";
+import rootReducer from '../reducers'
+import { logger, crashReporter } from "../middlewares/logger"
+import sagaMonitor from '../sagaMonitor'
+
+// preloadedState
+let preloadedState = {
+    todos: Utils.store('react-todos'),
+    showFilter: SHOW_ALL
+};
+
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware();
+
+export default function configureStore() {
+    const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
+    return {
+        ...createStore(
+            rootReducer,
+            preloadedState,
+            applyMiddleware(sagaMiddleware)),
+        runSaga: sagaMiddleware.run
+    }
+}
