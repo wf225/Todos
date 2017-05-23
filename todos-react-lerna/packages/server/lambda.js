70eebaa6ece12af6a2778aa7440758323c5b980e
Bill Wu <bill.wu@autodesk.com>
2017-05-23 19:05:41 +0800

    Clean code.

--- todos-react-lerna/packages/server/lambda.js
+++ todos-react-lerna/packages/server/lambda.js
@@ -1,8 +1,9 @@
 'use strict'
 const awsServerlessExpress = require('aws-serverless-express');
 
-require('./dist/app');
-const app = GLOBAL.app;
+require('./src/app');
+// require('./dist/app');
+// const app = GLOBAL.app;
 
 // NOTE: If you get ERR_CONTENT_DECODING_FAILED in your browser, this is likely
 // due to a compressed response (e.g. gzip) which has not been handled correctly
@@ -21,29 +22,3 @@ const binaryMimeTypes = [
 
 const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes);
 exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
-
-
-
-// // Test code
-// exports.handler = function (event, context, callback) {
-//   var name = "World";
-//   var responseCode = 200;
-//   console.log("request: " + JSON.stringify(event));
-//   var responseBody = {
-//     message: "Hello " + name + "!",
-//     input: event
-//   };
-//   var response = {
-//     statusCode: responseCode,
-//     headers: {
-//       // "Accept": "application/json",
-//       "Content-Type": "application/json",
-//       "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key",
-//       "Access-Control-Allow-Methods": "*",
-//       "Access-Control-Allow-Origin": "*"
-//     },
-//     body: responseBody // JSON.stringify(responseBody)
-//   };
-//   console.log("response: " + response)
-//   callback(null, response);
-// };
--- todos-react-lerna/packages/server/src/app.js
+++ todos-react-lerna/packages/server/src/app.js
@@ -5,15 +5,6 @@ const compression = require('compression')
 const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
 const app = express();
 
-// app.use(function (req, res, next) {
-//   // res.set('Accept', 'application/json');
-//   // res.set('Content-Type', 'application/json');
-//   // res.set('Access-Control-Allow-Headers', 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token');
-//   res.set('Access-Control-Allow-Methods', '*'); //GET,HEAD,PUT,PATCH,POST,DELETE
-//   res.set('Access-Control-Allow-Origin', '*');
-//   return next();
-// });
-
 // app.use(compression()) // TODO: Maybe AWS Lambda don't support gizp.
 app.use(cors())
 app.use(bodyParser.json())
